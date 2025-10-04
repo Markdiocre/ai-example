3268618016d89ddc272c310a0341be1980059075
Markdiocre <mark.manuel.business@gmail.com>
2025-10-04 08:31:22 +0800

    assasdas

--- movement/new_script.gd
+++ movement/new_script.gd
@@ -2,3 +2,26 @@ extends Node
 
 func _ready():
 	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	print("Moving")
+	
+	print("Moving")
+	
